365729bc3ed4bdc87facd3b8fee9be83d6d0b6f2
Radek Žilka <kradecek@gmail.com>
2013-11-14 22:59:54 +0100

    initial commit

--- tlapnet-konfigurator.php
+++ tlapnet-konfigurator.php
@@ -17,12 +17,11 @@ define('TLAPNET_KONFIGURATOR_PLUGIN_URL', plugin_dir_url(__FILE__));
 $parsedUrl = parse_url(TLAPNET_KONFIGURATOR_PLUGIN_URL);
 define('TLAPNET_KONFIGURATOR_PLUGIN_URL_PATH', $parsedUrl['path']);
 
-add_action( 'plugins_loaded', function() {
+add_action( 'plugins_loaded', 'Tlpanet_Konfigurator_Load_Plugin');
+
+function Tlpanet_Konfigurator_Load_Plugin() {
   require_once __DIR__ . '/classes/Plugin.php';
   $Tlapnet_Konfigurator = new Tlapnet\Konfigurator\Plugin();
   $Tlapnet_Konfigurator->init();  
-});
-
-
-
+}
 
